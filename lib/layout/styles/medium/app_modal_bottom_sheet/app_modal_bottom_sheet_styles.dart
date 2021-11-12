c01dca7aee1097930ce5819bbf7e9a613af388f9
joaopinacio <joaopedro.triun@gmail.com>
2021-11-12 16:42:22 -0300

    Correção de layout na selacao de quantidade do produto

--- lib/layout/styles/medium/app_modal_bottom_sheet/app_modal_bottom_sheet_styles.dart
+++ lib/layout/styles/medium/app_modal_bottom_sheet/app_modal_bottom_sheet_styles.dart
@@ -42,182 +42,183 @@ class AppModalBottomSheetStyles {
     required Color colorQty,
     required Color colorQtyIcons,
     required bool isUserRestaurant,
-  }) =>
-      showModalBottomSheetComponent(
-        radiusTopLeft: 34.r,
-        radiusTopRight: 34.r,
-        child: Container(
-          height: 560.h,
-          child: Stack(
-            children: [
-              Container(
-                height: 190.h,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(34.r), topRight: Radius.circular(34.r)),
-                  child: AppNetworkImageStyles.standard(
-                    behaviour: Behaviour.regular,
-                    image: image!,
-                    boxFit: BoxFit.cover,
-                    height: 100.h,
-                    width: double.infinity,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10.r),
-                      topRight: Radius.circular(10.r),
-                    ),
-                  ),
-                  // Image.asset(
-                  //   image ?? 'assets/images/image_food.png',
-                  //   fit: BoxFit.fitWidth,
-                  //   height: 132.h,
-                  //   width: double.infinity,
-                  // ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 398.h,
+  }) {
+    showModalBottomSheetComponent(
+      radiusTopLeft: 34.r,
+      radiusTopRight: 34.r,
+      child: Container(
+        height: 560.h,
+        child: Stack(
+          children: [
+            Container(
+              height: 190.h,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(34.r), topRight: Radius.circular(34.r)),
+                child: AppNetworkImageStyles.standard(
+                  behaviour: Behaviour.regular,
+                  image: image!,
+                  boxFit: BoxFit.cover,
+                  height: 100.h,
                   width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: AppThemes.colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(34.r),
-                      topRight: Radius.circular(34.r),
-                    ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.r),
+                    topRight: Radius.circular(10.r),
                   ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: AppThemes.spacing.spacer_20.h),
-                      AppTextStyles.semiBold_18(
-                        text: title,
-                        color: AppThemes.colors.black,
-                        margin: EdgeInsets.only(bottom: AppThemes.spacing.spacer_3.h),
+                ),
+                // Image.asset(
+                //   image ?? 'assets/images/image_food.png',
+                //   fit: BoxFit.fitWidth,
+                //   height: 132.h,
+                //   width: double.infinity,
+                // ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 398.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppThemes.colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(34.r),
+                    topRight: Radius.circular(34.r),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: AppThemes.spacing.spacer_20.h),
+                    AppTextStyles.semiBold_18(
+                      text: title,
+                      color: AppThemes.colors.black,
+                      margin: EdgeInsets.only(bottom: AppThemes.spacing.spacer_3.h),
+                    ),
+                    SizedBox(height: AppThemes.spacing.spacer_8.h),
+                    Container(
+                      width: price.length > 5 ? 205 : 165.w,
+                      height: 36.h,
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: price.length > 5 ? 145.w : 105.w,
+                            height: 36.h,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(34.r),
+                                bottomLeft: Radius.circular(34.r),
+                              ),
+                              color: AppThemes.colors.grayScale_3,
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 12.w),
+                              child: Row(
+                                children: [
+                                  AppTextStyles.semiBold_8(
+                                    text: "R\$",
+                                    color: AppThemes.colors.black,
+                                    margin: EdgeInsets.only(bottom: AppThemes.spacing.spacer_3.h),
+                                  ),
+                                  SizedBox(width: AppThemes.spacing.spacer_1.w),
+                                  AppTextStyles.semiBold_14(
+                                    text: price,
+                                    color: AppThemes.colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: AppCounterStyles.product(
+                              behaviour: Behaviour.regular,
+                              color: colorQty,
+                              colorIcons: colorQtyIcons,
+                              onChange: (value) {
+                                print(value);
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: AppThemes.spacing.spacer_8.h),
-                      Container(
-                        width: 165.w,
-                        height: 36.h,
-                        child: Stack(
+                    ),
+                    SizedBox(height: AppThemes.spacing.spacer_14.h),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24.w),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: AppTextStyles.medium_14(
+                          text: "Ingredients",
+                          color: AppThemes.colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: AppThemes.spacing.spacer_6.h),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: AppThemes.spacing.spacer_24.w),
+                      child: Container(
+                        height: 75.h,
+                        child: ListView(
+                          physics: BouncingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          // listFoodIngredients.map((item) { }).toList(),
                           children: [
-                            Container(
-                              width: 105.w,
-                              height: 36.h,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(34.r),
-                                  bottomLeft: Radius.circular(34.r),
-                                ),
-                                color: AppThemes.colors.grayScale_3,
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 12.w),
-                                child: Row(
-                                  children: [
-                                    AppTextStyles.semiBold_8(
-                                      text: "R\$",
-                                      color: AppThemes.colors.black,
-                                      margin: EdgeInsets.only(bottom: AppThemes.spacing.spacer_3.h),
-                                    ),
-                                    SizedBox(width: AppThemes.spacing.spacer_1.w),
-                                    AppTextStyles.semiBold_14(
-                                      text: price,
-                                      color: AppThemes.colors.black,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: AppCounterStyles.product(
-                                behaviour: Behaviour.regular,
-                                color: colorQty,
-                                colorIcons: colorQtyIcons,
-                                onChange: (value) {
-                                  print(value);
-                                },
-                              ),
-                            ),
+                            AppThemes.shimmer.circle(size: 75),
+                            SizedBox(width: AppThemes.spacing.spacer_10.w),
+                            AppThemes.shimmer.circle(size: 75),
+                            SizedBox(width: AppThemes.spacing.spacer_10.w),
+                            AppThemes.shimmer.circle(size: 75),
+                            SizedBox(width: AppThemes.spacing.spacer_10.w),
+                            AppThemes.shimmer.circle(size: 75),
+                            SizedBox(width: AppThemes.spacing.spacer_10.w),
+                            AppThemes.shimmer.circle(size: 75),
+                            SizedBox(width: AppThemes.spacing.spacer_10.w),
+                            AppThemes.shimmer.circle(size: 75),
                           ],
                         ),
                       ),
-                      SizedBox(height: AppThemes.spacing.spacer_14.h),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 24.w),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: AppTextStyles.medium_14(
-                            text: "Ingredients",
-                            color: AppThemes.colors.black,
-                          ),
+                    ),
+                    SizedBox(height: AppThemes.spacing.spacer_12.h),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24.w),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: AppTextStyles.medium_14(
+                          text: "Details",
+                          color: AppThemes.colors.black,
                         ),
                       ),
-                      SizedBox(height: AppThemes.spacing.spacer_6.h),
-                      Padding(
+                    ),
+                    SizedBox(height: AppThemes.spacing.spacer_4.h),
+                    AppTextStyles.regular_12(
+                      text: description,
+                      color: AppThemes.colors.black_50,
+                      maxLines: 3,
+                      margin: EdgeInsets.symmetric(horizontal: AppThemes.spacing.spacer_24.w),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Spacer(),
+                    Opacity(
+                      opacity: isUserRestaurant ? 0.3 : 1,
+                      child: Padding(
                         padding: EdgeInsets.symmetric(horizontal: AppThemes.spacing.spacer_24.w),
-                        child: Container(
-                          height: 75.h,
-                          child: ListView(
-                            physics: BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            // listFoodIngredients.map((item) { }).toList(),
-                            children: [
-                              AppThemes.shimmer.circle(size: 75),
-                              SizedBox(width: AppThemes.spacing.spacer_10.w),
-                              AppThemes.shimmer.circle(size: 75),
-                              SizedBox(width: AppThemes.spacing.spacer_10.w),
-                              AppThemes.shimmer.circle(size: 75),
-                              SizedBox(width: AppThemes.spacing.spacer_10.w),
-                              AppThemes.shimmer.circle(size: 75),
-                              SizedBox(width: AppThemes.spacing.spacer_10.w),
-                              AppThemes.shimmer.circle(size: 75),
-                              SizedBox(width: AppThemes.spacing.spacer_10.w),
-                              AppThemes.shimmer.circle(size: 75),
-                            ],
-                          ),
+                        child: AppTextButtonStyles.rounded(
+                          height: 57,
+                          width: double.infinity,
+                          hasBounce: isUserRestaurant ? false : true,
+                          textStyle: AppThemes.typography.poppinsBold_14,
+                          label: 'Add to Cart',
+                          onTap: isUserRestaurant ? () {} : onSave,
                         ),
                       ),
-                      SizedBox(height: AppThemes.spacing.spacer_12.h),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 24.w),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: AppTextStyles.medium_14(
-                            text: "Details",
-                            color: AppThemes.colors.black,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: AppThemes.spacing.spacer_4.h),
-                      AppTextStyles.regular_12(
-                        text: description,
-                        color: AppThemes.colors.black_50,
-                        maxLines: 3,
-                        margin: EdgeInsets.symmetric(horizontal: AppThemes.spacing.spacer_24.w),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Spacer(),
-                      Opacity(
-                        opacity: isUserRestaurant ? 0.3 : 1,
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: AppThemes.spacing.spacer_24.w),
-                          child: AppTextButtonStyles.rounded(
-                            height: 57,
-                            width: double.infinity,
-                            hasBounce: isUserRestaurant ? false : true,
-                            textStyle: AppThemes.typography.poppinsBold_14,
-                            label: 'Add to Cart',
-                            onTap: isUserRestaurant ? () {} : onSave,
-                          ),
-                        ),
-                      ),
-                      Spacer(),
-                    ],
-                  ),
+                    ),
+                    Spacer(),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
